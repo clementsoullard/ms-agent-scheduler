43f713f988c765785b704f5b9d80e64dc4f9ea6e
Clement <clementsoullard@yahoo.fr>
2016-09-09 22:21:09 +0200

    Première ébauche d'un service periodique

--- CheckProcess/Service1.cs
+++ CheckProcess/Service1.cs
@@ -12,6 +12,11 @@ namespace CheckProcess
 {
     public partial class Service1 : ServiceBase
     {
+        System.Threading.Thread processThread;
+        System.Timers.Timer timer;
+        private Boolean Cancel;
+
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +24,66 @@ namespace CheckProcess
 
         protected override void OnStart(string[] args)
         {
+            Debug.WriteLine("Demaarage");
+
+            timer = new System.Timers.Timer(5000);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Tick);
+            timer.Start();
+
+            Cancel = false;
+            // Get all processes running on the local computer.
+            Process[] localAll = Process.GetProcesses();
+        }
+        protected override void OnContinue()
+        {
+            timer.Start();
+        }
+
+        protected override void OnPause()
+        {
+            timer.Stop();
         }
 
         protected override void OnStop()
         {
+            if (processThread.ThreadState == System.Threading.ThreadState.Running)
+            {
+                Cancel = true;
+                // Give thread a chance to stop
+                processThread.Join(500);
+                processThread.Abort();
+            }
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            processThread = new System.Threading.Thread(new System.Threading.ThreadStart(DoWork));
+            processThread.Start();
+        }
+
+        private void DoWork()
+        {
+            try
+            {
+                while (!Cancel)
+                {
+
+                    if (Cancel) { return; }
+                    // Do General Work
+
+                    System.Threading.Thread.BeginCriticalRegion();
+                    {
+                        Debug.WriteLine("The product name is " );
+                    }
+                    System.Threading.Thread.EndCriticalRegion();
+                }
+
+            }
+            catch (System.Threading.ThreadAbortException tae)
+            {
+                // Clean up correctly to leave program in stable state.
+            }
         }
     }
+
+
 }
